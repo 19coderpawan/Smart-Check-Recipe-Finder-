40bb731cd85131e223903b3eb63d5eb08cc51c48
19coderpawan <coderpawankushwaha@gmail.com>
2025-11-12 01:10:30 +0530

    Setup the file structure for frontend

---
+++ Chef/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f97316",
+        darkbg: "#0f0f0f",
+      },
+    },
+  },
+  plugins: [],
+};
